b239ac8067b7222de29d8baf74edcfb0d41e6333
leonardolucasbs <leonardodlucasbri1@gmail.com>
2025-09-05 17:11:47 -0300

    refactor- :recycle: refatorando erro no tarefa6.py

--- dia2/tarefa6.py
+++ dia2/tarefa6.py
@@ -1,26 +1,41 @@
+class Animal:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def apresentar(self):
+        return f"Nome: {self.nome} | Idade: {self.idade}"
+
+    def falar(self):
+        pass
+
+
+class Gato(Animal):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+
+    def falar(self):
+        return "Miiiiaaaaaaauuuuu 🐱"
+    
+    
+class Cachorro(Animal):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+
+    def falar(self):
+        return "Au au au 🐶"
+
 
 class Zoologico:
     def __init__(self):
         self.animais = []
-
-    class Animal():
-        def __init__(self, nome, idade):
-            self.nome = nome
-            self.idade = idade  
-        
-        def falar(self):
-            pass
         
     def adicionar_animal(self, animal):
         self.animais.append(animal)
         
     def listar_animais(self):
         for animal in self.animais:
-            print(f"Nome: {animal.nome}, Idade: {animal.idade}, Som: {animal.falar()}")
+            print(f"{animal.apresentar()} | Som: {animal.falar()}")
             
     def filtrar_por_tipo(self, tipo):
         return [animal for animal in self.animais if isinstance(animal, tipo)]
-    
-
-        
-
